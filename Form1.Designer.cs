e3eb579f46c7b1a95b90dcdfd4089ed0cc2a9f6e
25020091032OsmanGurbuz <25020091032@ogr.istiklal.edu.tr>
2026-03-12 09:44:02 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,443 @@
+﻿namespace telefon_rehperi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox10 = new PictureBox();
+            pictureBox13 = new PictureBox();
+            pictureBox14 = new PictureBox();
+            pictureBox12 = new PictureBox();
+            pictureBox15 = new PictureBox();
+            pictureBox16 = new PictureBox();
+            pictureBox17 = new PictureBox();
+            pictureBox18 = new PictureBox();
+            pictureBox25 = new PictureBox();
+            button1 = new Button();
+            label7 = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            label8 = new Label();
+            pictureBox6 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox15).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox16).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox17).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox18).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox25).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Blue;
+            panel1.Controls.Add(pictureBox9);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(pictureBox4);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(pictureBox12);
+            panel1.Controls.Add(pictureBox13);
+            panel1.Controls.Add(pictureBox25);
+            panel1.Controls.Add(pictureBox14);
+            panel1.Controls.Add(pictureBox10);
+            panel1.Controls.Add(pictureBox8);
+            panel1.Controls.Add(pictureBox18);
+            panel1.Controls.Add(pictureBox17);
+            panel1.Controls.Add(pictureBox16);
+            panel1.Controls.Add(pictureBox7);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(pictureBox15);
+            panel1.Controls.Add(pictureBox5);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.ForeColor = Color.AliceBlue;
+            panel1.Location = new Point(457, 342);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(346, 574);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(91, 74);
+            label1.Name = "label1";
+            label1.Size = new Size(89, 38);
+            label1.TabIndex = 0;
+            label1.Text = "özcan";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(91, 188);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 38);
+            label2.TabIndex = 1;
+            label2.Text = "enis";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(72, 308);
+            label3.Name = "label3";
+            label3.Size = new Size(101, 38);
+            label3.TabIndex = 2;
+            label3.Text = "babam";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(72, 371);
+            label4.Name = "label4";
+            label4.Size = new Size(91, 38);
+            label4.TabIndex = 3;
+            label4.Text = "yakup";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(72, 246);
+            label5.Name = "label5";
+            label5.Size = new Size(86, 38);
+            label5.TabIndex = 4;
+            label5.Text = "kader";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(52, 135);
+            label6.Name = "label6";
+            label6.Size = new Size(198, 38);
+            label6.TabIndex = 5;
+            label6.Text = "aptullah taştan";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.telefon_simgesi;
+            pictureBox1.Location = new Point(277, 87);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(40, 38);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.telefon_simgesi;
+            pictureBox3.Location = new Point(277, 371);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(40, 38);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 7;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.telefon_simgesi;
+            pictureBox5.Location = new Point(277, 308);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(40, 38);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 9;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = Properties.Resources.telefon_simgesi;
+            pictureBox7.Location = new Point(277, 246);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(40, 38);
+            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox7.TabIndex = 11;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = Properties.Resources.telefon_simgesi;
+            pictureBox8.Location = new Point(277, 188);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(40, 38);
+            pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox8.TabIndex = 12;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.Image = Properties.Resources.telefon_simgesi;
+            pictureBox10.Location = new Point(277, 135);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(40, 38);
+            pictureBox10.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox10.TabIndex = 14;
+            pictureBox10.TabStop = false;
+            // 
+            // pictureBox13
+            // 
+            pictureBox13.Image = Properties.Resources.telefon_simgesi;
+            pictureBox13.Location = new Point(277, 432);
+            pictureBox13.Name = "pictureBox13";
+            pictureBox13.Size = new Size(40, 38);
+            pictureBox13.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox13.TabIndex = 17;
+            pictureBox13.TabStop = false;
+            // 
+            // pictureBox14
+            // 
+            pictureBox14.Image = Properties.Resources.insan_simgesi;
+            pictureBox14.Location = new Point(0, 74);
+            pictureBox14.Name = "pictureBox14";
+            pictureBox14.Size = new Size(46, 42);
+            pictureBox14.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox14.TabIndex = 18;
+            pictureBox14.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            pictureBox12.Image = Properties.Resources.insan_simgesi;
+            pictureBox12.Location = new Point(0, 242);
+            pictureBox12.Name = "pictureBox12";
+            pictureBox12.Size = new Size(46, 42);
+            pictureBox12.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox12.TabIndex = 19;
+            pictureBox12.TabStop = false;
+            // 
+            // pictureBox15
+            // 
+            pictureBox15.Image = Properties.Resources.insan_simgesi;
+            pictureBox15.Location = new Point(0, 184);
+            pictureBox15.Name = "pictureBox15";
+            pictureBox15.Size = new Size(46, 42);
+            pictureBox15.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox15.TabIndex = 20;
+            pictureBox15.TabStop = false;
+            // 
+            // pictureBox16
+            // 
+            pictureBox16.Image = Properties.Resources.insan_simgesi;
+            pictureBox16.Location = new Point(0, 304);
+            pictureBox16.Name = "pictureBox16";
+            pictureBox16.Size = new Size(46, 42);
+            pictureBox16.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox16.TabIndex = 21;
+            pictureBox16.TabStop = false;
+            // 
+            // pictureBox17
+            // 
+            pictureBox17.Image = Properties.Resources.insan_simgesi;
+            pictureBox17.Location = new Point(0, 367);
+            pictureBox17.Name = "pictureBox17";
+            pictureBox17.Size = new Size(46, 42);
+            pictureBox17.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox17.TabIndex = 22;
+            pictureBox17.TabStop = false;
+            // 
+            // pictureBox18
+            // 
+            pictureBox18.Image = Properties.Resources.insan_simgesi;
+            pictureBox18.Location = new Point(0, 428);
+            pictureBox18.Name = "pictureBox18";
+            pictureBox18.Size = new Size(46, 42);
+            pictureBox18.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox18.TabIndex = 23;
+            pictureBox18.TabStop = false;
+            // 
+            // pictureBox25
+            // 
+            pictureBox25.Image = Properties.Resources.insan_simgesi;
+            pictureBox25.Location = new Point(0, 131);
+            pictureBox25.Name = "pictureBox25";
+            pictureBox25.Size = new Size(46, 42);
+            pictureBox25.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox25.TabIndex = 30;
+            pictureBox25.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 128, 128);
+            button1.Location = new Point(3, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(347, 43);
+            button1.TabIndex = 31;
+            button1.Text = "Rehber";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(91, 432);
+            label7.Name = "label7";
+            label7.Size = new Size(53, 38);
+            label7.TabIndex = 32;
+            label7.Text = "......";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.insan_simgesi;
+            pictureBox2.Location = new Point(0, 488);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(46, 42);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 33;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.telefon_simgesi;
+            pictureBox4.Location = new Point(277, 492);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(40, 38);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 34;
+            pictureBox4.TabStop = false;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(72, 492);
+            label8.Name = "label8";
+            label8.Size = new Size(146, 38);
+            label8.TabIndex = 35;
+            label8.Text = "sebastiyan";
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(453, 342);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(50, 43);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 36;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.Image = (Image)resources.GetObject("pictureBox9.Image");
+            pictureBox9.Location = new Point(305, 0);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(41, 46);
+            pictureBox9.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox9.TabIndex = 36;
+            pictureBox9.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(15F, 37F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1924, 1055);
+            Controls.Add(pictureBox6);
+            Controls.Add(panel1);
+            Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            Margin = new Padding(6, 6, 6, 6);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox15).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox16).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox17).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox18).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox25).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox14;
+        private PictureBox pictureBox13;
+        private PictureBox pictureBox10;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox3;
+        private Label label7;
+        private Button button1;
+        private PictureBox pictureBox12;
+        private PictureBox pictureBox25;
+        private PictureBox pictureBox18;
+        private PictureBox pictureBox17;
+        private PictureBox pictureBox16;
+        private PictureBox pictureBox15;
+        private Label label8;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox6;
+    }
+}
